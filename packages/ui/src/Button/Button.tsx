11899630e45edaf54fc59924618418b2f4d9055c
Clement GASTON <clement.gaston@viacesi.fr>
2022-06-09 21:32:56 +0200

    fix: rectify eslint config

--- packages/ui/src/Button/Button.tsx
+++ packages/ui/src/Button/Button.tsx
@@ -1,7 +1,6 @@
 import { ReactElement } from "react";
 import s from "./Button.module.scss";
 
-// eslint-disable-next-line import/prefer-default-export
 export const Button = (): ReactElement => (
   <button className={s.test} type="button">
     Boop
