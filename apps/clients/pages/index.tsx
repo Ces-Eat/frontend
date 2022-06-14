c3dd2a3e29cf4138f728aca6e4e9378713da137a
Clément GASTON <clement.gaston@viacesi.fr>
2022-06-14 14:21:44 +0200

    fix: package rename ui -> cel

--- apps/clients/pages/index.tsx
+++ apps/clients/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { Button, Card } from "ui";
+import { Button, Card } from "cel";
 import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => (
