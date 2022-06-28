b894fcc194bed8f9854c5b14b0954a0c0f22b8d1
Clement GASTON <clement.gaston@viacesi.fr>
2022-06-28 15:27:40 +0200

    build: dev tiers website

---
+++ dev-tier/pages/_app.tsx
@@ -0,0 +1,13 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { NotificationProvider } from "@ceseatslib/utils";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <NotificationProvider>
+      <Component {...pageProps} />
+    </NotificationProvider>
+  );
+}
+
+export default MyApp;
